67fb386b01056a626f9b161db14919975764c117
gokula <gokulakrishnanr812@gmail.com>
2025-08-04 12:32:48 +0530

    Refactor Firebase configuration to use environment variables: replace hardcoded values with process.env variables for improved security and flexibility in the firebase.ts file.

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,13 +5,13 @@ import { getAnalytics, isSupported } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCym4ODf9LmNiJi7oOXiUNmBd5DsNjUHPI",
-  authDomain: "macwrite-280d0.firebaseapp.com",
-  projectId: "macwrite-280d0",
-  storageBucket: "macwrite-280d0.firebasestorage.app",
-  messagingSenderId: "199900747857",
-  appId: "1:199900747857:web:79a4ae9a3267015fb68070",
-  measurementId: "G-EP9Z7Q8920"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
